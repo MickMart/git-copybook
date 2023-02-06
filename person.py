8edab86d263b9034b474b5b889ed7700df8ffa4f
MickMart <bouncing.man@yandex.ru>
2023-02-06 15:12:09 +0700

    Correction: correction book and person.

--- person.py
+++ person.py
@@ -4,20 +4,25 @@ import pydantic
 
 
 class Name(pydantic.BaseModel):
-    last_name: str
+    first_name: str
     second_name: str
-    otechstvo: str
+    middle_name: str
 
 
 class Contact(pydantic.BaseModel):
-    home: str
-    work: str
+    home: str = ''
+    work: str = ''
 
 
 class Mail(Contact):
 
     @pydantic.validator('home', 'work')
     def is_correct(cls, value):
+        if value != '':
+            if re.match(
+                    f'^((([0-9A-Za-z]{1}[-0-9A-z\.]{0, 30}[0-9A-Za-z]?)|([0-9А-Яа-я]{1}[-0-9А-я\.]{0, 30}[0-9А-Яа-я]?))@([-A-Za-z]{1,}\.){1,}[-A-Za-z]{2,})$',
+                    value):
+                print('Is not correct mail!')
         return value
 
 
@@ -25,6 +30,9 @@ class Phone(Contact):
 
     @pydantic.validator('home', 'work')
     def is_correct(cls, value):
+        if value != '':
+            if re.match(f'/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10, 14}(\s*)?$/', value):
+                print('Is not correct phone!')
         return value
 
 
